941bb03597abc890eaca2f73232a2f3e464f273d
Giorgi <stichy.dl@gmail.com>
2022-12-27 04:19:22 +0400

    navlinks styles changed

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -2,7 +2,7 @@ import NavLink from './NavLink'
 
 export default function NavLinks({ planets }) {
   return (
-    <div className="hidden md:flex gap-[33px] items-center font-spartan">
+    <div className="hidden md:flex gap-[33px] items-center font-spartan font-bold">
       {planets.map((planet) => (
         <NavLink planet={planet} key={planet.name} />
       ))}
